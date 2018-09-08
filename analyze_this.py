7bd4d7415517f315bda01c9947894859581eac3a
Matthew-Mesa <matthewmesa@gmail.com>
2018-09-08 14:13:42 -0700

    feat: Finished getPopAuthors() functionality

--- analyze_this.py
+++ analyze_this.py
@@ -23,18 +23,45 @@ def getMostPop():
   db.close()
 
   #loop through the results and print them row-by-row
-  pops = 'The three most popular articles of all time: \n\n'
+  pops = 'The three most popular articles of all time: \n'
   for row in articles:
       title = row[1]
-      views = row[2]
-      #coding=utf-8
+      views = "{:,}".format(row[2])
       pops += u"\"%s\" - %s views \n" % (title, views)
-      #pops += row[1] + str('-') + row[2] + "views \n"
+
+  pops += "\n\n";
 
   return pops
 
 def getPopAuthors():
-    """ Return  """
+    """ Return a list of authors sorted by views """
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(""" select authors.name, SUM(views) as total_views from articles,
+      (select count(*) as views, replace(path, '/article/', '') as slug from log
+        where status = '200 OK'
+            and path != '/'
+        group by slug
+        order by views desc) as log,
+        authors
+            where log.slug = articles.slug
+            and articles.author = authors.id
+            group by authors.name
+            order by total_views desc;
+    """)
+    articles = c.fetchall()
+    db.close()
+
+    #loop through the results and print them row-by-row
+    author_views = 'Most popular article authors of all time: \n'
+    for row in articles:
+        name = row[0]
+        views = "{:,}".format(row[1])
+        #coding=utf-8
+        author_views += u"%s - %s views \n" % (name, views)
+
+    return author_views
 
 
 def exportResults():
@@ -42,4 +69,5 @@ def exportResults():
 
     f = open('results.txt', 'w')
     f.write(getMostPop())
+    f.write(getPopAuthors())
     f.close()
